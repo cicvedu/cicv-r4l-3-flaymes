629dbf8c00f9941704ac2404a925dcf859b52f3e
Flaymes <59189242+flaymes@users.noreply.github.com>
2024-07-22 23:18:11 +0800

    Update rust_chrdev.rs

--- linux/samples/rust/rust_chrdev.rs
+++ linux/samples/rust/rust_chrdev.rs
@@ -40,11 +40,35 @@ impl file::Operations for RustFile {
     }
 
     fn write(_this: &Self,_file: &file::File,_reader: &mut impl kernel::io_buffer::IoBufferReader,_offset:u64,) -> Result<usize> {
-        Err(EPERM)
+        let mut buf = _this.inner.lock();
+        let offset = _offset as usize;
+        let total_len = _reader.len();
+
+        if offset>=GLOBALMEM_SIZE{
+            return Ok(0);
+        }
+
+        let _n_bytes=core::cmp::min(total_len,GLOBALMEM_SIZE-offset);
+        _reader.read_slice(&mut buf[offset..offset+_n_bytes])?;
+        return Ok(_n_bytes);
+
     }
 
     fn read(_this: &Self,_file: &file::File,_writer: &mut impl kernel::io_buffer::IoBufferWriter,_offset:u64,) -> Result<usize> {
-        Err(EPERM)
+        let buffer=_this.inner.lock();
+        let total_len = _writer.len();
+        let offset = _offset as usize;
+
+        if offset>=GLOBALMEM_SIZE{
+            return Ok(0);
+        }
+
+        while !_writer.is_empty() {
+            let _n_bytes=core::cmp::min(total_len,GLOBALMEM_SIZE-offset);
+            _writer.write_slice(&buffer[offset..offset+_n_bytes])?;
+        }
+        Ok(total_len)
+
     }
 }
 
@@ -56,13 +80,14 @@ impl kernel::Module for RustChrdev {
     fn init(name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
         pr_info!("Rust character device sample (init)\n");
 
-        let mut chrdev_reg = chrdev::Registration::new_pinned(name, 0, module)?;
+        let mut chrdev_reg = chrdev::Registration::new_pinned(name, 248, module)?;
 
         // Register the same kind of device twice, we're just demonstrating
         // that you can use multiple minors. There are two minors in this case
         // because its type is `chrdev::Registration<2>`
-        chrdev_reg.as_mut().register::<RustFile>()?;
-        chrdev_reg.as_mut().register::<RustFile>()?;
+        chrdev_reg.as_mut().register::<RustFile>(0)?;
+        chrdev_reg.as_mut().register::<RustFile>(1)?;
+
 
         Ok(RustChrdev { _dev: chrdev_reg })
     }
